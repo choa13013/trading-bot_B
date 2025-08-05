573fb3df7c5bb4f380a737c62f1997c79d89f471
choa13013 <deleuze.fr@gmail.com>
2025-08-06 01:35:25 +0200

    main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import time
 from binance.client import Client
 from dotenv import load_dotenv
 
-load_dotenv()
-
 API_KEY = os.getenv("BINANCE_API_KEY")
 API_SECRET = os.getenv("BINANCE_API_SECRET")
 
